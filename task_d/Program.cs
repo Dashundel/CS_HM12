0694a7032a5fbcc197978c61c59c35be757f53a8
Dasha <steshitc@mail.ru>
2022-04-19 17:25:27 +0300

    add code

---
+++ task_d/Program.cs
@@ -0,0 +1,23 @@
+﻿//D. Наименьшее число
+/*
+Дана последовательность чисел. Найти в ней наименьшее число.
+
+Входные данные
+Сначала задано число N - количество чисел в последовательности, а затем N чисел (1 <  = N <  = 100).
+
+Выходные данные
+Выведите наименьшее число.
+*/
+
+Console.WriteLine("Введи кол-во чисел: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введи последовательность чисел: ");
+
+int[] arr = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+int min = arr[0];
+
+for (int i = 1; i < n; i++)
+	if(arr[i] < min) min = arr[i];		
+	
+System.Console.Write(min);
